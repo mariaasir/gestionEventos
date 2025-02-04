6398393ae17be73cdde27c61e5e3226c7824922e
mariaasir <maria.l.paton@gmail.com>
2025-02-04 09:22:37 +0100

    Creación del proyecto

---
+++ gestionEventos/src/main/java/demo/gestioneventos/GestionEventosApplication.java
@@ -0,0 +1,13 @@
+package demo.gestioneventos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionEventosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GestionEventosApplication.class, args);
+    }
+
+}
---
+++ gestionEventos/src/test/java/demo/gestioneventos/GestionEventosApplicationTests.java
@@ -0,0 +1,13 @@
+package demo.gestioneventos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionEventosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
